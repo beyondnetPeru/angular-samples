bb6202961b805c21e3ad42e2ed2752d8b8f2009c
alberto.arroyo <beyondnet.peru@gmail.com>
2023-03-06 16:54:03 -0500

    adding new sample

--- rxjs-samples/src/app/samples/samples.component.ts
+++ rxjs-samples/src/app/samples/samples.component.ts
@@ -16,6 +16,7 @@ import {
 } from "rxjs";
 import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
+import { Timestamp } from "rxjs/internal-compatibility";
 
 @Component({
   selector: "app-samples",
@@ -25,13 +26,26 @@ import { createHttpObservable } from "../common/util";
 export class SamplesComponent implements OnInit {
   enabledButton = true;
   textvalue: "";
+  showPassword = false;
+  clicks: Array<string>;
 
   ngOnInit() {
     this.textvalue = "";
-    this.enabledButton = false;
+    this.clicks = new Array<string>();
   }
 
   onInputTextEvent() {
     this.enabledButton = this.textvalue.length ? true : false;
+  }
+
+  onDisplayDetails() {
+    this.showPassword = !this.showPassword;
+
+    // this.clicks.push(`${this.clicks.length + 1}`);
+    this.clicks.push(`${new Date()}`);
+  }
+
+  getColor() {
+    return this.clicks.length > 3 ? "blue" : "white";
   }
 }
